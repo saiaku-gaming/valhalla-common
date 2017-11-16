30ae0413f5ee57d785aa537838ffb087bf3a30b6
Mio Engel <mioengel@gmail.com>
2017-11-16 19:41:20 +0100

    inital commit

---
+++ src/main/java/com/valhallagame/common/JS.java
@@ -0,0 +1,54 @@
+package com.valhallagame.ymer.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Simple utility helper for converting stuff to json.
+ */
+public class JS {
+
+	private static ObjectMapper mapper = new ObjectMapper();
+
+	public JS() {
+
+	}
+
+	public static ResponseEntity<?> message(HttpStatus status, String message) {
+		return ResponseEntity.status(status).body(JS.message(message));
+	}
+
+	public static ResponseEntity<?> message(HttpStatus status, Object o) {
+		return ResponseEntity.status(status).body(JS.parse(o));
+	}
+
+	public static JsonMessage message(String message) {
+		return new JsonMessage(message);
+	}
+
+	public static JsonNode parse(Object o) {
+		return mapper.valueToTree(o);
+	}
+
+	public static class JsonMessage {
+		private String message;
+
+		public JsonMessage() {
+		}
+
+		public JsonMessage(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+	}
+}
