49de382b4b9d6e3d99cf3e6c83c52fb534eb28ed
Mio Engel <mioengel@gmail.com>
2017-11-21 22:38:34 +0100

    added notification service

--- src/main/java/com/valhallagame/common/DefaultServicePortMappings.java
+++ src/main/java/com/valhallagame/common/DefaultServicePortMappings.java
@@ -6,4 +6,5 @@ public class DefaultServicePortMappings {
 	public static int FRIEND_SERVICE_PORT = 1236;
 	public static int PARTY_SERVICE_PORT = 1237;
 	public static int CHARACTER_SERVICE_PORT = 1238;
+	public static int NOTIFICATION_SERVICE_PORT = 1239;
 }
