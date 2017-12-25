5e6adb1b1d8dd47b5797615cd202c818facd65ee
Mikael Berglund <mikbe@kth.se>
2017-12-25 11:12:02 +0100

    More pleasing the linter

--- src/main/java/com/valhallagame/common/DefaultServicePortMappings.java
+++ src/main/java/com/valhallagame/common/DefaultServicePortMappings.java
@@ -1,14 +1,17 @@
 package com.valhallagame.common;
 
 public class DefaultServicePortMappings {
-	public static int YMER_SERVICE_PORT = 1234;
-	public static int PERSON_SERVICE_PORT = 1235;
-	public static int FRIEND_SERVICE_PORT = 1236;
-	public static int PARTY_SERVICE_PORT = 1237;
-	public static int CHARACTER_SERVICE_PORT = 1238;
-	public static int NOTIFICATION_SERVICE_PORT = 1239;
-	public static int INSTANCE_CONTAINER_SERVICE_PORT = 1240;
-	public static int WARDROBE_SERVICE_PORT = 1241;
-	public static int CHAT_SERVICE_PORT = 1242;
-	public static int INSTANCE_SERVICE_PORT = 1243;
+	private DefaultServicePortMappings(){
+		//NO construct
+	}
+	public static final int YMER_SERVICE_PORT = 1234;
+	public static final int PERSON_SERVICE_PORT = 1235;
+	public static final int FRIEND_SERVICE_PORT = 1236;
+	public static final int PARTY_SERVICE_PORT = 1237;
+	public static final int CHARACTER_SERVICE_PORT = 1238;
+	public static final int NOTIFICATION_SERVICE_PORT = 1239;
+	public static final int INSTANCE_CONTAINER_SERVICE_PORT = 1240;
+	public static final int WARDROBE_SERVICE_PORT = 1241;
+	public static final int CHAT_SERVICE_PORT = 1242;
+	public static final int INSTANCE_SERVICE_PORT = 1243;
 }
