5580f4c72e3c34fad4c380839efbc18af325f81a
Mikael Berglund <mikbe@kth.se>
2017-12-25 11:29:08 +0100

    More pleasing the linter

--- src/main/java/com/valhallagame/common/rabbitmq/RabbitMQRouting.java
+++ src/main/java/com/valhallagame/common/rabbitmq/RabbitMQRouting.java
@@ -1,39 +1,39 @@
 package com.valhallagame.common.rabbitmq;
 
 public class RabbitMQRouting {
-	public static enum Exchange {
+	public enum Exchange {
 		PERSON, FRIEND, PARTY, CHAT, WARDROBE, INSTANCE_CONTAINER, INSTANCE, CHARACTER
 	}
 
-	public static enum InstanceContainer {
+	public enum InstanceContainer {
 		CREATE, READY
 	}
 
-	public static enum Instance {
+	public enum Instance {
 		DUNGEON_ACTIVE, PERSON_LOGIN, PERSON_LOGOUT
 	}
 
-	public static enum Person {
+	public enum Person {
 		DELETE, CREATE, ONLINE, OFFLINE
 	}
 
-	public static enum Friend {
+	public enum Friend {
 		ADD, REMOVE, RECEIVED_INVITE, DECLINE_INVITE
 	}
 
-	public static enum Party {
+	public enum Party {
 		CANCEL_INVITE, ACCEPT_INVITE, DECLINE_INVITE, LEAVE, PROMOTE_LEADER, KICK_FROM_PARTY, SENT_INVITE, RECEIVED_INVITE
 	}
 
-	public static enum Chat {
+	public enum Chat {
 		RECEIVED_MESSAGE
 	}
 
-	public static enum Wardrobe {
+	public enum Wardrobe {
 		ADD_WARDROBE_ITEM
 	}
 
-	public static enum Character {
+	public enum Character {
 		DELETE
 	}
 }
